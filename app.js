237d57396152f944e8647eea97ea549dc4aea2e9
Sena Çelik <sena.celik@miateknoloji.com>
2023-09-30 21:47:17 +0300

    codereview

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(cors());
 app.use(express.json({limit:'30mb', extended:true}))
 app.use(express.urlencoded({limit:'30mb', extended:true}))
 
+
+app.get('/', (req, res) => res.send('Hello World!' ))
 app.use('/', User)
 
 
@@ -26,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 db()
 
 app.listen( PORT, () =>{
-      console.log("Server is running on port: 5000")
+      console.log("Server is running on port: 3000")
 })
 
 
