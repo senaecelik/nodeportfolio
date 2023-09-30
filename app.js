3d93e149129e529140fb870f2d309f2e2dbc2d93
Sena Çelik <sena.celik@miateknoloji.com>
2023-09-30 20:49:34 +0300

    completed portfolio restfull api

--- app.js
+++ app.js
@@ -1,17 +1,35 @@
 
 
+//importlama
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv')
+const db = require('./config/database')
+const User = require("./routes/user.js")
 
-const express = require("express")
+
+
+dotenv.config();
+
+
 const app = express();
+app.use(cors());
+app.use(express.json({limit:'30mb', extended:true}))
+app.use(express.urlencoded({limit:'30mb', extended:true}))
 
-app.listen(3000, ()=>{
-      console.log("Api is running on port 3000")
+app.use('/', User)
+
+
+const PORT = process.env.PORT || 5000;
+
+
+db()
+
+app.listen( PORT, () =>{
+      console.log("Server is running on port: 5000")
 })
 
 
-app.get('/',(req, res)=>{
-      res.send('Hello node api')
-});
 
 
 
@@ -40,75 +58,86 @@ app.get('/',(req, res)=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const fs = require("fs/promises");
-
-
-// const express = require("express")
-// const cors = require("cors");
-// const _ = require("lodash");
-// const {v4: uuid} = require("uuid");
-
-
-// const app = express();
 // app.use(express.json());
-// app.get("/outfit",(req, res)=>{
-//       const tops = ["Black","White","Orange"];
-//       const jeans = ["Grey"];
 
-//       res.json({
-//             tops: _.sample(tops),
-//             jeans: _.sample(jeans)
 
-//       });
-// })
+// const User = require("./models/userModel")
+// const username = encodeURIComponent("admin");
+// const password = encodeURIComponent("Udbhy1278");
 
-// app.post("/comments",async (req, res) => {
-//       const id = uuid();
-//       const content = req.body.content;
 
-//       if(!content){
-//             return res.sendStatus(400);
-//       }
-
-//       await fs.mkdir("data/comments", {recursive:true});
-//       await fs.writeFile(`data/comments/${id}.txt`, content)
-
-//       res.status(201).json( {
-//                 id: id
-//             }
-//       )
-// })
-
-// app.get("/comments/:id",async  (req,res) =>{
-
-//       const id = req.params.id;
-//       let content;
-
+// app.post('/user',async (req, res)=>{
 //       try {
-//            content = await fs.readFile(`data/comments/${id}.txt`,"utf-8");
 
+//             const user = await User.create(req.body);
+//             res.status(200).json(user);
+          
 //       } catch (error) {
-//            return res.sendStatus(404)
+//             res.status(500).json({message:error.message});
 //       }
+   
+// });
 
-//       res.json({
-//             content: content
+// app.get('/users',async (req, res)=>{
+//       try {
+//             const user = await User.find({});
+//             res.status(200).json(user);
+//       } catch (error) {
+//             res.status(500).json({message:error.message});
+//       }
+// });
+
+// app.get('/users/:id',async(req, res)=>{
+//       try {
+//             const {id} = req.params
+//             const user = await User.findByID(id);
+//             res.status(200).json(user);
+//       } catch (error) {
+//             res.status(500).json({message:error.message});
+//       }
+// });
+
+
+// mongoose.connect(`mongodb+srv://${username}:${password}@portfolioapi.ughb95k.mongodb.net/Portfolio-API?retryWrites=true&w=majority`)
+// .then(()=>{
+//       console.log('connected to Mongo DB');
+//       app.listen( port, ()=>{
+//             console.log("Api is running on port 5000");
 //       })
-
+// }).catch((error)=>{
+//       console.log(error);
 // })
 
-// app.listen(3000, ()=> console.log("API Server is running..."))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
